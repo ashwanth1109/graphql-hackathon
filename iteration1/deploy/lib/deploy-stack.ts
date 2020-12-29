b8c668e1218099a7bd39670231d40c1d2dd125e7
Ashwanth A R <ashwanth1109@gmail.com>
2020-12-29 10:40:09 +0530

    deploy: add appsync api with basic schema

--- iteration1/deploy/lib/deploy-stack.ts
+++ iteration1/deploy/lib/deploy-stack.ts
@@ -1,9 +1,41 @@
-import * as cdk from '@aws-cdk/core';
+import {
+  App,
+  CfnOutput,
+  Duration,
+  Expiration,
+  Stack,
+  StackProps,
+} from "@aws-cdk/core";
+import { AuthorizationType, GraphqlApi, Schema } from "@aws-cdk/aws-appsync";
 
-export class DeployStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+export class DeployStack extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const api = new GraphqlApi(this, "Api", {
+      name: "cdk-appsync-test-api",
+      schema: Schema.fromAsset("../graphql/schema.graphql"),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: AuthorizationType.API_KEY,
+          apiKeyConfig: {
+            expires: Expiration.after(Duration.days(365)),
+          },
+        },
+      },
+      xrayEnabled: true,
+    });
 
+    new CfnOutput(this, "GraphQLAPIURL", {
+      value: api.graphqlUrl,
+    });
+
+    new CfnOutput(this, "GraphQLAPIKey", {
+      value: api.apiKey || "",
+    });
+
+    new CfnOutput(this, "Stack Region", {
+      value: this.region,
+    });
   }
 }
