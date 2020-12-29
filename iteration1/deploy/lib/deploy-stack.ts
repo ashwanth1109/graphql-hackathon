a77138ad9950a512841f2c0de10e943f48eba2ba
Ashwanth A R <ashwanth1109@gmail.com>
2020-12-29 11:58:26 +0530

    major: iteration complete

--- iteration1/deploy/lib/deploy-stack.ts
+++ iteration1/deploy/lib/deploy-stack.ts
@@ -7,6 +7,8 @@ import {
   StackProps,
 } from "@aws-cdk/core";
 import { AuthorizationType, GraphqlApi, Schema } from "@aws-cdk/aws-appsync";
+import { Code, Function, Runtime } from "@aws-cdk/aws-lambda";
+import { AttributeType, BillingMode, Table } from "@aws-cdk/aws-dynamodb";
 
 export class DeployStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
@@ -37,5 +39,50 @@ export class DeployStack extends Stack {
     new CfnOutput(this, "Stack Region", {
       value: this.region,
     });
+
+    const notesLambda = new Function(this, "AppSyncNotesHandler", {
+      runtime: Runtime.NODEJS_12_X,
+      handler: "main.handler",
+      code: Code.fromAsset("../lambda-fns"),
+      memorySize: 1024,
+    });
+
+    const lambdaDs = api.addLambdaDataSource("lambdaDataSource", notesLambda);
+
+    lambdaDs.createResolver({
+      typeName: "Query",
+      fieldName: "getNoteById",
+    });
+
+    lambdaDs.createResolver({
+      typeName: "Query",
+      fieldName: "listNotes",
+    });
+
+    lambdaDs.createResolver({
+      typeName: "Mutation",
+      fieldName: "createNote"
+    });
+
+    lambdaDs.createResolver({
+      typeName: "Mutation",
+      fieldName: "deleteNote",
+    });
+
+    lambdaDs.createResolver({
+      typeName: "Mutation",
+      fieldName: "updateNote",
+    });
+
+    const notesTable = new Table(this, "CDKNotesTable", {
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      partitionKey: {
+        name: "id",
+        type: AttributeType.STRING,
+      },
+    });
+
+    notesTable.grantFullAccess(notesLambda);
+    notesLambda.addEnvironment("NOTES_TABLE", notesTable.tableName);
   }
 }
