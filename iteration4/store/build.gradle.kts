3a1babb297f3f88ba72db4cd762f93890588a116
Ashwanth A R <ashwanth1109@gmail.com>
2021-01-01 10:18:00 +0530

    store.service: add deploy setup for store api

---
+++ iteration4/store/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    java
+    application
+}
+
+group = "com.gql.services.store"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+var cdkVersion = "1.74.0"
+
+dependencies {
+    testCompile("junit", "junit", "4.12")
+
+    // This dependency is used by the application.
+    implementation(group = "software.amazon.awscdk", name = "core", version = cdkVersion)
+    implementation(group = "software.amazon.awscdk", name = "appsync", version = cdkVersion)
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+application {
+    // Define the main class for the application.
+    mainClassName = "com.gql.services.store.Manager"
+}
---
+++ iteration4/store/src/main/java/com/gql/services/store/deploy/stacks/ServiceStack.java
@@ -0,0 +1,10 @@
+package com.gql.services.store.deploy.stacks;
+
+import software.amazon.awscdk.core.Construct;
+import software.amazon.awscdk.core.Stack;
+
+public class ServiceStack extends Stack {
+    public ServiceStack(final Construct parent, String id) {
+        super(parent, id);
+    }
+}
